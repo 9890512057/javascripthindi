bce5ab18fa5537e78e49ac2434a012b3c427a881
Abhinav Kutwal <54620245+9890512057@users.noreply.github.com>
2023-07-04 05:23:43 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId=12345
+let accountEmail="abhinav@00..com"
+var accountPassword="12345"
+accountCity="pune"
+console.table([  accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,11 @@
+// alert.console.log();("abhinav")
+let a=99999999999999999999999999n;
+console.log(typeof a)
+
+//number
+// String
+// bigint
+// boolean
+// null
+// null
+// object
