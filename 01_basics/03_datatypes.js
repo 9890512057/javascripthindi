40806139a4756e781139d56b791cf7f3d45f80e5
Abhinav Kutwal <54620245+9890512057@users.noreply.github.com>
2023-10-04 04:06:01 +0000

    datatypes done

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,10 @@
+let a="Abhinav";
+let b=22.5567898;
+let c=null;
+let d=undefined;
+let e=true;
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
