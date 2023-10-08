bd8259cb762742ecfd71fc69ed9e25863cc6519b
Abhinav Kutwal <54620245+9890512057@users.noreply.github.com>
2023-10-08 06:31:51 +0000

    stack heap

---
+++ 01_basics/10_stackheap.js
@@ -0,0 +1,17 @@
+let aemail="Abhinav@gmail";
+let aanothermail=aemail;
+ aanothermail="kutwalabhinav";
+
+ console.log(aanothermail);
+ console.log(aemail);
+
+ let  user ={
+    mail:"kutwalabhinav9696",
+    upi :"abhinav@ybl"
+ }
+
+ let usertwo=user;
+ usertwo.mail="prasad";
+ console.log(user);
+ console.log(user.mail);
+ console.log(usertwo.mail);
