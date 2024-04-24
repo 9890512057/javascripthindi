94a0dd57a6ba3a3b0209cd9820641d5d2853239e
Abhinav Kutwal <54620245+9890512057@users.noreply.github.com>
2024-04-24 02:08:52 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId="abhinav";  // values cannot be change in code it is constant
+let email="abhinav@gmail.com";
+var pass="kutwal";//now we does not var because it does not uses scope and let uses let
+ accountCity="baramati";
+//
+
+console.table({accountId,email,pass,accountCity});
+//console.log(accountId);
