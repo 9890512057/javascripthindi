4790cb03ae39ffafd72a51c6ff994046143464f5
Abhinav Kutwal <54620245+9890512057@users.noreply.github.com>
2024-04-24 02:44:58 +0000

    conversion operation

---
+++ 01_basics/03_convOp.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let aname= "Abhinav";
+let Sname= Number(aname); // Corrected function name to Number
+console.log(typeof(sname));
+console.log(Sname);   // NAN not a number
+ // Uncomment this line if you want to log the type of `aname`
+let age="23";
+let nage=Number(age);
+console.log(typeof(nage));
+console.log(nage);
+let Isadult=1;
+let ak=Boolean(Isadult);
+console.log(ak);
+
+
