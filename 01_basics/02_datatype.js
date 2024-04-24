9d5fa78ab62289cf2e52745a6ffa0f49825072ad
Abhinav Kutwal <54620245+9890512057@users.noreply.github.com>
2024-04-24 02:09:10 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,13 @@
+"use strict";
+let a="Abhinav";
+let b=13;
+let c=3.4;
+let d;
+let e=null;
+
+//Object
+console.log(typeof(e));
+console.log(typeof(d));
+console.log(typeof(c));
+console.log(typeof(b));
+console.log(typeof(a));
