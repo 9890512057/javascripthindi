0c71301d1c6770e8dbf3db93bc57ba6de54639a5
Abhinav Kutwal <54620245+9890512057@users.noreply.github.com>
2024-04-25 04:18:44 +0000

    number and math

---
+++ 01_basics/06_number.js
@@ -0,0 +1,12 @@
+let otp=Math.floor(Math.random() *10000000000+1);
+console.log();
+let max=10000;
+let min=99999;
+let a=(Math.floor(Math.random()*(max-min+1)+min));
+
+if (otp==otp) {
+    console.log(otp);
+    console.log("Sucessfull");
+}else{
+    console.log("failed");
+}
